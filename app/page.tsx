1545faf36f0403cc40d18b1b8ac22524ca92b592
KaSeM5 <kasem200mohareb@gmail.com>
2024-06-03 21:24:37 +0300

    message

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Projects from "./component/Projects";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start px-24">
+    <main className="flex min-h-screen flex-col items-center justify-start px-5 md:px-24">
      <Info/>
      <Projects/>
     </main>
