6a431f455ec5a6a8ae26ccc207093b93e0c632f7
KaSeM5 <kasem200mohareb@gmail.com>
2024-06-04 21:27:28 +0300

    update

--- app/component/Info.tsx
+++ app/component/Info.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div className='p-2  md:text-lg  bg-orange-600 rounded-b-md underline'> The website is currently under development, and I&apos;m focused on creating an exceptional portfolio project. At present, I&apos;ve built the following websites, with many more to come in the upcoming days.</div>
+    <div className='p-2  md:text-lg  bg-orange-500 rounded-b-md underline'> The website is currently under development, and I&apos;m focused on creating an exceptional portfolio project. At present, I&apos;ve built the following websites, with many more to come in the upcoming days&#129395;&#129395;.</div>
   )
 }
 
