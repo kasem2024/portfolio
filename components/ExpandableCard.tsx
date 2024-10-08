46f1261e77484bba607a58472294f87dde80170a
kasem2023mohareb@gmail.com <kasem2023mohareb@gmail.com>
2024-10-09 01:58:00 +0300

    fix

--- components/ExpandableCard.tsx
+++ components/ExpandableCard.tsx
@@ -180,50 +180,50 @@ export const CloseIcon = () => {
 
 const cards = [
   {
-    description: "Matlab",
+    description: "Matlap",
     title: "Image Processing",
     ctaText: "Open",
     content: () => {
       return (
         <p>
-          Project Overview:
-          <br />
-          During my studies at Damnhour University, I collaborated with a team to develop an advanced image processing system using MATLAB. The project was built to automatically detect and enhance edges in various images, improving visual clarity and detail.
-          <br />
-          Technical Details:
-          <br />
-          Algorithms: We implemented several image processing algorithms, such as edge detection, filtering, and image segmentation, to enhance the quality and clarity of the images.
-          Tools and Libraries: Utilizing MATLAB&apos;s powerful image processing toolbox, we efficiently manipulated pixel data and applied various transformations.
-          Challenges Overcome: One of the significant challenges we tackled was optimizing the algorithms to handle large image datasets without compromising performance. This required careful consideration of computational efficiency and memory usage.
-          Impact:
-          <br />
-          The project demonstrated the potential of MATLAB in real-world applications and laid the groundwork for further research in image processing. Our work not only enhanced our technical skills but also provided valuable insights into the practical challenges of working with complex image data.
+         Project Overview:
+         <br/>
+During my studies at Damnhour University, I collaborated with a team to develop an advanced image processing system using MATLAB. The project was built to automatically detect and enhance edges in various images, improving visual clarity and detail.
+<br/>
+Technical Details:
+<br/>
+Algorithms: We implemented several image processing algorithms, such as edge detection, filtering, and image segmentation, to enhance the quality and clarity of the images.
+Tools and Libraries: Utilizing MATLAB&apos;s powerful image processing toolbox, we efficiently manipulated pixel data and applied various transformations.
+Challenges Overcome: One of the significant challenges we tackled was optimizing the algorithms to handle large image datasets without compromising performance. This required careful consideration of computational efficiency and memory usage.
+Impact:<br/>
+The project demonstrated the potential of MATLAB in real-world applications and laid the groundwork for further research in image processing. Our work not only enhanced our technical skills but also provided valuable insights into the practical challenges of working with complex image data.
+
+
         </p>
       );
     },
   },
   {
-    description: "Python",
+    description: "Python ",
     title: "Chat System",
     ctaText: "Open",
     content: () => {
       return (
         <p>
-          Project Overview:
-          <br />
-          As part of a team project at Damnhour University, we developed a robust chat system using Python. The system was designed to facilitate real-time communication between users, incorporating various features that enhanced user experience and ensured secure data transmission.
-          <br />
-          Technical Details:
-          <br />
-          Programming Language: We utilized Python as the primary language, leveraging its simplicity and power to implement both the front-end and back-end of the chat application.
-          Libraries and Tools: The system was built using essential Python libraries like socket for handling connections and threading for managing multiple users simultaneously. We also employed encryption techniques to secure messages and protect user privacy.
-          Challenges Overcome: One of the key challenges was ensuring real-time performance while maintaining scalability. We implemented efficient data handling and optimized the server to manage numerous concurrent connections without latency issues.
-          <br /> Impact:
-          <br />
-          The project demonstrated our ability to build a fully functional, scalable chat system from scratch. It provided valuable insights into real-time communication protocols, data security, and the importance of user-centric design. This experience significantly enhanced our understanding of network programming and multi-threading in Python.
-        </p>
-      );
-    },
+         Project Overview:
+         <br/>
+As part of a team project at Damnhour University, we developed a robust chat system using Python. The system was designed to facilitate real-time communication between users, incorporating various features that enhanced user experience and ensured secure data transmission.
+<br/>
+Technical Details:
+<br/>
+Programming Language: We utilized Python as the primary language, leveraging its simplicity and power to implement both the front-end and back-end of the chat application.
+Libraries and Tools: The system was built using essential Python libraries like socket for handling connections and threading for managing multiple users simultaneously. We also employed encryption techniques to secure messages and protect user privacy.
+Challenges Overcome: One of the key challenges was ensuring real-time performance while maintaining scalability. We implemented efficient data handling and optimized the server to manage numerous concurrent connections without latency issues.
+<br/> Impact: <br/>
+The project demonstrated our ability to build a fully functional, scalable chat system from scratch. It provided valuable insights into real-time communication protocols, data security, and the importance of user-centric design. This experience significantly enhanced our understanding of network programming and multi-threading in Python.
+</p>
+    );
+  },
   },
   {
     description: "Spring Boot (Java)",
@@ -232,18 +232,17 @@ const cards = [
     content: () => {
       return (
         <p>
-          Project Overview:
-          <br />
-          As part of a collaborative project at Damnhour University, we developed an online Tic-Tac-Toe game, utilizing Spring Boot (Java) for the backend and JavaScript for the front end. The game was designed to offer a seamless and interactive experience for players, allowing them to compete in real-time.
-          <br />
-          Technical Details:
-          <br />
-          Backend Development: We used Spring Boot to create a robust and scalable backend. The server handled game logic, player sessions, and real-time communication between clients, ensuring a smooth gaming experience.
-          Frontend Development: The front end was built using JavaScript, providing a dynamic and responsive interface. Players could easily interact with the game board, and the interface updated in real-time based on the moves made by each player.
-          Challenges Overcome: One of the significant challenges was ensuring the synchronization of game states between multiple players. We implemented efficient WebSocket communication to handle real-time data exchange, ensuring that both players saw the same game state without delays.
-          <br /> Impact:
-          <br />
-          The project showcased our ability to integrate front-end and back-end technologies to build a complete, functional web application. It also provided valuable experience in real-time data handling, user interface design, and the use of Spring Boot for managing server-side logic.
+        Project Overview:
+        <br/>
+As part of a collaborative project at Damnhour University, we developed an online Tic-Tac-Toe game, utilizing Spring Boot (Java) for the backend and JavaScript for the front end. The game was designed to offer a seamless and interactive experience for players, allowing them to compete in real-time.
+
+Technical Details: <br/>
+
+Backend Development: We used Spring Boot to create a robust and scalable backend. The server handled game logic, player sessions, and real-time communication between clients, ensuring a smooth gaming experience.
+Frontend Development: The front end was built using JavaScript, providing a dynamic and responsive interface. Players could easily interact with the game board, and the interface updated in real-time based on the moves made by each player.
+Challenges Overcome: One of the significant challenges was ensuring the synchronization of game states between multiple players. We implemented efficient WebSocket communication to handle real-time data exchange, ensuring that both players saw the same game state without delays.
+<br/> Impact: <br/>
+The project showcased our ability to integrate front-end and back-end technologies to build a complete, functional web application. It also provided valuable experience in real-time data handling, user interface design, and the use of Spring Boot for managing server-side logic.
         </p>
       );
     },
@@ -255,24 +254,131 @@ const cards = [
     content: () => {
       return (
         <p>
-          Project Overview:
-          <br />
-          In one of my recent projects, I developed a competitive online chat platform that featured real-time messaging, utilizing Next.js for the backend and Pusher to handle real-time data transmission. The chat system was designed to provide a seamless and responsive communication experience, catering to users who require instant interaction.
-          <br />
-          Technical Details:
-          <br />
-          Backend Development: Next.js was chosen for its flexibility and powerful features, making it ideal for managing real-time messaging. The server managed user authentication, message storage, and delivered messages instantly between users.
-          Real-Time Messaging: Pusher was integrated to enable real-time communication, ensuring that messages were delivered and displayed instantly without requiring manual refreshes. This created a dynamic and engaging user experience.
-          Challenges Overcome: One of the primary challenges was optimizing the system to handle a large number of concurrent users while maintaining low latency. We implemented efficient message queues and optimized database interactions to ensure the platform could scale without compromising performance.
-          <br /> Impact:
-          <br />
-          This project demonstrated my ability to build a high-performance, real-time communication platform from scratch. It provided hands-on experience with real-time data handling, user authentication, and the integration of third-party services like Pusher to enhance the user experience.
+      Project Overview: <br/>
+      In one of my recent projects, I developed a competitive online chat platform that featured real-time messaging, utilizing Next.js for the backend and Pusher to handle real-time data transmission. The chat system was designed to provide a seamless and responsive communication experience, catering to users who require instant interaction.
+      <br/>
+      Technical Details:<br/>
+      Backend Development: Next.js was chosen for its flexibility and powerful features, making it ideal for managing real-time messaging. The server managed user authentication, message storage, and delivered messages instantly between users.
+      Real-Time Messaging: Pusher was integrated to enable real-time communication, ensuring that messages were delivered and displayed instantly without requiring manual refreshes. This created a dynamic and engaging user experience.
+      Challenges Overcome: One of the primary challenges was optimizing the system to handle a large number of concurrent users while maintaining low latency. We implemented efficient message queues and optimized database interactions to ensure the platform could scale without compromising performance.
+      <br/>Impact:<br/>
+      This project demonstrated my ability to build a high-performance, real-time communication platform from scratch. It provided hands-on experience with real-time data handling, user authentication, and the integration of third-party services like Pusher to enhance the user experience.
+    </p>
+      );
+    },
+  },
+  {
+    description: "PHP & JavaScript & Bootstrap",
+    title: "E-commerce",
+    ctaText: "Open",
+    content: () => {
+      return (
+        <p>
+  Project Overview:
+  <br/>
+As a backend developer on a college team, I contributed to building a feature-rich E-commerce platform using PHP, JavaScript, Bootstrap, and MySQL. The platform included essential features like product listings, a shopping cart, user profiles, and secure payment processing.
+<br/>
+Key Contributions:
+<br/>
+Backend Development: Developed core features such as user authentication, product uploads, shopping cart functionality, and payment processing.
+Database Management: Designed and managed the MySQL database, optimizing data storage and retrieval for users, products, and orders.
+Integration: Worked closely with the front-end team to ensure seamless interaction between the backend and the user interface, creating a smooth and responsive shopping experience.
+<br/>Impact:<br/>
+This project honed my skills in backend development, database management, and creating secure, user-friendly applications.
         </p>
       );
     },
   },
-  // The rest of the cards follow the same pattern
+  {
+    description: "Python",
+    title: "License Plate Recognition",
+    ctaText: "Open",
+    content: () => {
+      return (
+        <p>
+         Project Overview:<br/>
+I developed a license plate recognition system using Python, with a focus on creating a user-friendly and clear interface. The project aimed to accurately detect and recognize license plates from images, providing users with a seamless experience.
+<br/>
+Key Contributions:
+<br/>
+Algorithm Development: Implemented image processing and machine learning techniques to detect and recognize license plates with high accuracy.
+User Interface: Designed and built a clean, intuitive UI, ensuring that users could easily upload images, view results, and navigate the system.
+Optimization: Focused on optimizing the processing time to deliver quick and reliable results without compromising accuracy.
+<br/>
+Impact:
+<br/>
+This project enhanced my skills in Python, image processing, and UI design, demonstrating my ability to build practical applications with a strong focus on user experience.
+        </p>
+      );
+    },
+  },
+  {
+    description: "Machine Learning",
+    title: "Object Recognition System",
+    ctaText: "Open",
+    content: () => {
+      return (
+        <p>
+         Project Overview:<br/>
+         As part of a collaborative project at Damnhour University, I developed an object recognition system using Python. The project aimed to accurately identify and classify objects within images, enhancing automated analysis and decision-making.
+         <br/>
+Key Contributions:<br/>
+
+Algorithm Development: Implemented object detection and classification algorithms to achieve high accuracy in identifying various objects.
+System Integration: Collaborated on integrating the recognition system into a user-friendly application, allowing for easy image uploads and result visualization.
+Optimization: Worked on optimizing the system for speed and efficiency, ensuring prompt and reliable object recognition.
+<br/>Impact:<br/>
+This project demonstrated my ability to work in a team to build a sophisticated object recognition system, enhancing my skills in Python, machine learning, and practical application development.
+        </p>
+      );
+    },
+  },
+  {
+    description: "OpenAi & Replicate",
+    title: "AI Media Generator",
+    ctaText: "Open",
+    content: () => {
+      return (
+        <p>
+Project Overview:<br/>
+As the team leader for our graduation project, I led the development of an AI media generator that utilized OpenAI and Replicate AI to create images, audio, video, conversations, and music based on user input. I was responsible for overseeing this area of the project and ensuring its successful implementation.
+<br/>
+Key Contributions:
+<br/>
+Integration: Led the integration of OpenAI and Replicate AI APIs to enable the generation of diverse media types, including images, audio, and video.
+Feature Development: Directed the development of features for generating and customizing media content, focusing on delivering a responsive and intuitive user experience.
+Optimization: Managed the optimization of AI models and API interactions to ensure high-quality and timely media generation.
+<br/>Impact:<br/>
+This project showcased my leadership and technical skills in integrating advanced AI technologies and developing a multimedia application, delivering a seamless experience across various media formats.
+        </p>
+      );
+    },
+  },
+  {
+    description: "HTML CSS JS",
+    title: "Interactive web pages",
+    ctaText: "Open",
+    content: () => {
+      return (
+        <p>
+        Project Overview:<br/>
+I have extensive experience building interactive web pages, utilizing HTML, CSS, and JavaScript to create dynamic and engaging user experiences. These projects involved developing responsive designs, implementing complex interactions, and optimizing performance for a wide range of devices and browsers.
+<br/>
+Key Contributions:<br/>
+
+Web Development: Designed and built responsive, mobile-friendly websites with interactive features, including animations, user inputs, and real-time updates.
+Performance Optimization: Focused on optimizing the performance of web pages to ensure fast loading times and smooth user experiences across different devices.
+Cross-Browser Compatibility: Ensured compatibility with major browsers, providing consistent experiences regardless of the user's platform.
+<br/>Impact:<br/>
+These projects demonstrated my ability to create visually appealing, functional web pages that prioritize user engagement and performance, showcasing my proficiency in front-end development.
+        </p>
+      );
+    },
+  },
 ];
+
+
+
 
 // const cards = [
 //   {
