4c37c75f0ab385899edaf58938e704643b8ffc5d
kasem2023mohareb@gmail.com <kasem2023mohareb@gmail.com>
2024-10-09 00:29:07 +0300

    last change

--- components/blocks/expandable-card-demo-standard.tsx
+++ components/blocks/expandable-card-demo-standard.tsx
@@ -2,7 +2,8 @@
 import Image from "next/image";
 import React, { useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { useOutsideClick } from "@/hooks/use-outside-click";
+import { useOutsideclick } from "@/hooks/use-outside-click";
+import { useOutsideClick } from "@/hooks/useOutsideclick";
 
 export default function ExpandableCardDemo() {
   const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
